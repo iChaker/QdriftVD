ce2b307a3f170101ebd09e92363ff7a15f34813c
Iyadh Chaker <chaker.iyadh@gmail.com>
2025-02-14 14:15:38 +0100

    density matrices and distances

--- Qdrift.py
+++ Qdrift.py
@@ -11,7 +11,7 @@ def evolve_Qdrift(Hdict,total_time,nsamples,seed=1,initial_state=None):
     Hterms=Hdict["terms"]
     probabilities=Hdict["probabilities"]
 
-    np.random.seed(seed)
+    np.random.seed(int(seed))
 
     dt = total_time * lambda_Qdrift / nsamples
     sampled_terms = np.random.choice(len(Hterms), size=nsamples, p=probabilities)
@@ -22,8 +22,41 @@ def evolve_Qdrift(Hdict,total_time,nsamples,seed=1,initial_state=None):
 
     return evolved_state
 
-def evolve_Qdrift_trajectories(Hdict,total_time,nsamples,ntrajs,initial_state=None,idx=None):
-    evolved_trajs=np.zeros((ntrajs,Hdict["hilbert_dim"]),dtype=complex)
+def evolve_Qdrift_fast(Hdict, total_time, nsamples, seed=1, initial_state=None):
+    # If no initial state is provided, use the default one
+    if initial_state is None:
+        initial_state = Hdict["initial_state"]
+        
+    lambda_Qdrift = Hdict["lambda"]
+    Hterms = Hdict["terms"]
+    probabilities = Hdict["probabilities"]
+
+    # Initialize random seed
+    np.random.seed(int(seed))
+
+    # Time step calculation
+    dt = total_time * lambda_Qdrift / nsamples
+
+    # Precompute the evolution matrices (using Taylor approximation)
+    c = np.cos(dt) * np.eye(len(initial_state))
+    s = np.sin(dt)
+    evol_mat = [(c - 1.j * s * Hterms[jj]) for jj in range(len(Hterms))]  # U = 1 - iHt (Taylor approx.)
+
+    # Sample Hamiltonian terms
+    sampled_terms = np.random.choice(len(Hterms), size=nsamples, p=probabilities)
+
+    evolved_state = initial_state.copy()
+    for i in range(nsamples):
+        evolved_state = evol_mat[sampled_terms[i]].dot(evolved_state)
+
+    return evolved_state
+
+def evolve_Qdrift_trajectories(Hdict,total_time,nsamples,ntrajs,initial_state=None,idx=None,fast=True):
+    evolved_trajs=np.zeros((ntrajs,Hdict["Hilbert_dim"]),dtype=complex)
     for traj in range(ntrajs):
-       evolved_trajs[traj]=evolve_Qdrift(Hdict,total_time,nsamples,traj*nsamples*total_time,initial_state)
+       if fast:
+        evolved_trajs[traj]=evolve_Qdrift_fast(Hdict,total_time,nsamples,int(traj*nsamples*total_time),initial_state)
+       else:
+        evolved_trajs[traj]=evolve_Qdrift(Hdict,total_time,nsamples,int(traj*nsamples*total_time),initial_state)
+
     return evolved_trajs,idx
